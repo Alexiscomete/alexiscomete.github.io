bf2e53e5bec15830710813c6253e917638bdddde
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-05-08 14:37:25 +0200

    fin js navbar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -126,3 +126,44 @@ let navbarData = {
         }
     ]
 }
+
+let url = window.location.href;
+let lang = url.includes("/en") ? "en" : url.includes("/fr") ? "fr" : "dibi";
+
+// je génère maintenant le menu
+
+let menu = document.createElement("div");
+menu.classList.add("navbar");
+
+navbarData[lang].forEach(element => {
+    let menuItem = document.createElement("div");
+    menuItem.classList.add("navbar-item");
+    // le div pour le nom du menu dans un lien
+    let menuItemLink = document.createElement("a");
+    menuItemLink.href = element.link;
+    let menuItemLinkText = document.createElement("div");
+    menuItemLinkText.classList.add("navbar-item-text");
+    menuItemLinkText.innerText = element.name;
+    menuItemLink.appendChild(menuItemLinkText);
+    menuItem.appendChild(menuItemLink);
+    // le div pour les sous-menu
+    let menuItemSub = document.createElement("div");
+    menuItemSub.classList.add("navbar-item-sub");
+    element.sub.forEach(subElement => {
+        let menuItemSubItem = document.createElement("div");
+        menuItemSubItem.classList.add("navbar-item-sub-item");
+        let menuItemSubItemLink = document.createElement("a");
+        menuItemSubItemLink.href = subElement.link;
+        let menuItemSubItemLinkText = document.createElement("div");
+        menuItemSubItemLinkText.classList.add("navbar-item-sub-item-text");
+        menuItemSubItemLinkText.innerText = subElement.name;
+        menuItemSubItemLink.appendChild(menuItemSubItemLinkText);
+        menuItemSubItem.appendChild(menuItemSubItemLink);
+        menuItemSub.appendChild(menuItemSubItem);
+    });
+    menuItem.appendChild(menuItemSub);
+    menu.appendChild(menuItem);
+});
+
+// ajout du menu au html
+document.body.appendChild(menu);
